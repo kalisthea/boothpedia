6e567ea097a539eb9cd281bd33bba802e43a2a56
Callista Chow <callistachow30@gmail.com>
2024-12-20 21:07:22 +0700

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -30,6 +30,9 @@ class EventController extends Controller
         // return view('index', ['events'=>$eventData]);
     }
 
+    function createevent(){
+        return view("createevent");
+    }
 
   
 }
